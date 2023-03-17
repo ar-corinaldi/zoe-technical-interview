4bfc42a4cb922dfa5e1c6d2de03d33f148d84623
Allan <allan9899@gmail.com>
2023-03-17 17:25:46 -0500

    Navbar done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,33 +1,10 @@
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
+import ZoeLogo from "../assets/image 33.png";
 
 function Navbar() {
   return (
-    <AppBar position="static" sx={{ width: "100%", height: "8vh" }}>
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              display: { xs: "none", md: "flex" },
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            LOGO
-          </Typography>
-        </Toolbar>
-      </Container>
-    </AppBar>
+    <nav className="primary-color" style={{ width: "100%", height: "80px" }}>
+      <img src={ZoeLogo} className="zoe-logo" />
+    </nav>
   );
 }
 export default Navbar;
