6ce753089d9bbc0cea333e5bb115f3b4f2d28ae3
Allan <allan9899@gmail.com>
2023-03-18 20:41:44 -0500

    Card Component

---
+++ src/components/Card.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { IAgent } from "../interfaces";
+import { Avatar, Grid, useMediaQuery, useTheme } from "@mui/material";
+
+function Card(props: IAgent) {
+  const theme = useTheme();
+  const isUpToSm = useMediaQuery(theme.breakpoints.up("sm"));
+
+  if (!isUpToSm) {
+    return (
+      <Grid
+        container
+        xs={12}
+        padding="16px"
+        sx={{
+          background: "#FFFFFF",
+          boxShadow: "0px 8px 16px rgba(29, 35, 58, 0.1)",
+          borderRadius: "12px",
+        }}
+      >
+        <Grid item xs={4}>
+          <Avatar
+            style={{ width: "112px", height: "112px" }}
+            src={props.avatar}
+          />
+        </Grid>
+        <Grid container item xs={8}>
+          <Grid item xs={12}>
+            <h4 className="card-name no-gutters">{props.name}</h4>
+          </Grid>
+          <Grid item xs={12}>
+            <h5 className="card-id no-gutters">ID: {props.id}</h5>
+          </Grid>
+          <Grid item xs={12}>
+            <div className="income-label">
+              Income: <span className="bold"> ${props.income}</span>
+            </div>
+          </Grid>
+        </Grid>
+      </Grid>
+    );
+  }
+
+  return (
+    <div className="card secondary-color">
+      <div className="card-body">
+        <Avatar
+          style={{ width: "112px", height: "112px" }}
+          src={props.avatar}
+        />
+        <div style={{ display: "flex", flexDirection: "column", gap: "8px" }}>
+          <h4 className="card-name no-gutters">{props.name}</h4>
+          <h5 className="card-id no-gutters">ID: {props.id}</h5>
+        </div>
+      </div>
+      <footer className="card-footer income-label">
+        Income: <span className="bold"> ${props.income}</span>
+      </footer>
+    </div>
+  );
+}
+
+export default Card;
