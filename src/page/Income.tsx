c687d3d3988d1bce84dd6ac13276448d828ef186
Allan <allan9899@gmail.com>
2023-03-18 20:42:59 -0500

    income page

--- src/page/Income.tsx
+++ src/page/Income.tsx
@@ -1,10 +1,11 @@
-import { Button, Grid, TextField, Typography } from "@mui/material";
 import React from "react";
-import GroupsIcon from "@mui/icons-material/Groups";
-import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
-import { Controller, useForm } from "react-hook-form";
+import LeftArrowIcon from "../assets/left-arrow.png";
+import GroupIcon from "../assets/group-icon.svg";
+import DollarIcon from "../assets/Currency.png";
+import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
+import { Grid } from "@mui/material";
 
 const schema = z.object({
   income: z.number().max(99_999).min(10_000),
@@ -21,77 +22,129 @@ type IcomePageProps = {
 };
 
 function IncomePage(props: IcomePageProps) {
-  const { control, handleSubmit } = useForm({
+  const { handleSubmit, setValue, formState } = useForm({
     defaultValues,
     resolver: zodResolver(schema),
   });
 
   const onSuccess = (values: any) => {
-    console.log(values);
     props.setIncome(values.income);
     props.setPageState("match");
   };
 
-  const onError = (err: any) => {
-    console.log(err);
-  };
+  const hasIncomeError = Boolean(formState.errors.income);
 
   return (
-    <Grid
-      container
-      justifyContent="center"
-      textAlign="center"
-      gap={2}
-      width="60%"
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        textAlign: "center",
+        gap: "56px",
+        alignContent: "center",
+        width: "100%",
+      }}
     >
-      <Grid item xs={12}>
-        <GroupsIcon fontSize="large" />
-      </Grid>
-      <Grid item xs={12}>
-        <Typography variant="h4">Find the best agent for you!</Typography>
-      </Grid>
-      <Grid item xs={12}>
-        <Typography variant="body1">
+      <div className="container" style={{ gap: "16px", alignItems: "center" }}>
+        <img src={GroupIcon} style={{ width: "90px", height: "63px" }} />
+        <h3 className="title no-gutters" style={{ marginTop: "8.5px" }}>
+          Find the best agent for you!
+        </h3>
+        <h5 className="subtitle-2 no-gutters">
           Fill the information below to get your matches!
-        </Typography>
-      </Grid>
-      <Grid item xs={12} mt={3}>
-        <form onSubmit={handleSubmit(onSuccess, onError)}>
-          <Grid container alignContent="center" justifyContent="center">
-            <Grid item xs={5}>
-              <Typography align="left">Current Income</Typography>
-              <Controller
-                name="income"
-                control={control}
-                render={({ field }) => (
-                  <TextField
-                    {...field}
-                    onChange={(e) => {
-                      const numberValue = Number(e.target.value);
-                      if (isNaN(numberValue)) return;
-                      field.onChange(Number(numberValue));
-                    }}
-                    hiddenLabel
-                    variant="outlined"
-                    fullWidth
-                    InputProps={{ startAdornment: <i>$</i> }}
-                  />
-                )}
+        </h5>
+      </div>
+      <form onSubmit={handleSubmit(onSuccess)} className="form">
+        <Grid
+          container
+          style={{
+            width: "364px",
+            alignSelf: "center",
+            gap: "40px",
+            display: "flex",
+          }}
+        >
+          <Grid
+            item
+            xs={12}
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "start",
+              alignItems: "start",
+              gap: "10px",
+            }}
+          >
+            <label
+              style={{
+                color: hasIncomeError ? "#d32f2f" : "#5F5F5F",
+                fontSize: "14px",
+                fontWeight: 400,
+                lineHeight: "16px",
+              }}
+            >
+              {hasIncomeError
+                ? formState.errors.income?.message
+                : "Current Income"}
+            </label>
+            <div
+              style={{
+                lineHeight: "1.4375em",
+                position: "relative",
+              }}
+            >
+              <img
+                src={DollarIcon}
+                style={{
+                  position: "absolute",
+                  width: "10.13px",
+                  left: "15px",
+                  top: "38%",
+                }}
               />
-            </Grid>
-            <Grid item xs={12} mt={3}>
-              <Button
-                variant="contained"
-                type="submit"
-                endIcon={<ArrowForwardIcon />}
-              >
-                Get matches
-              </Button>
-            </Grid>
+              <input
+                type="number"
+                onChange={(e) => {
+                  const valueNumber = Number(e.target.value);
+                  if (isNaN(valueNumber)) return;
+                  setValue("income", valueNumber);
+                }}
+                name="income"
+                style={{
+                  lineHeight: "1.4375em",
+                  height: "46px",
+                  width: "320px",
+                  padding: "10px",
+                  paddingLeft: "2rem",
+                  border: "1px solid #DCDCDC",
+                  fontSize: "16px",
+                  borderRadius: "4px",
+                  borderColor: hasIncomeError ? "#d32f2f" : "inherit",
+                }}
+              />
+            </div>
           </Grid>
-        </form>
-      </Grid>
-    </Grid>
+          <Grid item xs={12} style={{ width: "inherit", textAlign: "end" }}>
+            <button type="submit" className="button-contained">
+              <label style={{ position: "relative", right: "5px" }}>
+                Get matches
+              </label>
+              <img
+                src={LeftArrowIcon}
+                style={{
+                  position: "relative",
+                  width: "15.78px",
+                  height: "15.56px",
+                  left: "11.78px",
+                  top: "2px",
+                }}
+              />
+            </button>
+          </Grid>
+        </Grid>
+      </form>
+    </div>
   );
 }
 
