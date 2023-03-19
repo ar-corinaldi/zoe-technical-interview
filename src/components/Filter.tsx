d8dfbfadfbec6a766090223641825646e755775f
Allan <allan9899@gmail.com>
2023-03-18 20:41:54 -0500

    Filter component

---
+++ src/components/Filter.tsx
@@ -0,0 +1,47 @@
+import { MenuItem, Select, useMediaQuery, useTheme } from "@mui/material";
+import React from "react";
+
+const FILTER_OPTIONS = [
+  "Name (A-Z)",
+  "ID",
+  "Income: High first",
+  "Income: Low first",
+  "(Default) Income: Closest first",
+] as const;
+
+export type FilterTypes = typeof FILTER_OPTIONS[number];
+
+type FilterProps = {
+  filter: FilterTypes;
+  setFilter: (x: FilterTypes) => void;
+};
+
+function Filter(props: FilterProps) {
+  const theme = useTheme();
+  const isUpToSm = useMediaQuery(theme.breakpoints.up("sm"));
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "start",
+        gap: "9px",
+      }}
+    >
+      <label className="caption">Order agents by</label>
+      <Select
+        value={props.filter}
+        onChange={(e) => props.setFilter(e.target.value as FilterTypes)}
+        fullWidth={!isUpToSm}
+      >
+        {FILTER_OPTIONS.map((option) => (
+          <MenuItem key={option} value={option}>
+            {option}
+          </MenuItem>
+        ))}
+      </Select>
+    </div>
+  );
+}
+
+export default Filter;
